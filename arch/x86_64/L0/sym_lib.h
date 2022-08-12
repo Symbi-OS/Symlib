2cdcb837f815464afc9397f514f7556f39bb92b0
arlo albelli <arloalb@bu.edu>
2022-08-12 08:11:42 -0400

    replaced wrgsbase with wrmsr

--- arch/x86_64/L0/sym_lib.h
+++ arch/x86_64/L0/sym_lib.h
@@ -6,10 +6,10 @@
 
 /* #include "L0/sym_lib.h" */
 #define ARLO_GS
-
+//TODO: make this save for migrated processes. As it stands a migrated symbiote will have a gs base pointer into the wrong core
 #ifdef ARLO_GS
 #define GET_KERN_GS_CLOBBER_USER_GS                           \
-  asm("movl $0xc0000102, %%ecx; rdmsr; shl $0x20, %%rdx; add %%rdx, %%rax; wrgsbase %%rax; movq %%rax, %0; sti;" :"=rm"(kern_gs) : :"%rax", "%edx", "%ecx");
+  asm("movl $0xc0000102, %%ecx; rdmsr; movl $0xc0000101, %%ecx; wrmsr; sti;" :: :"%rax", "%edx", "%ecx");
 #else
 // get onto kern gs
 // Store kern gs
