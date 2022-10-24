3071c35c004aa5477a0e75499d5f8b7fe17c7a37
FlareCoding <albert.slepak@gmail.com>
2022-10-23 22:10:05 -0400

    removed the dependency on kallsymlib

--- include/LIDK/idk.h
+++ include/LIDK/idk.h
@@ -2,7 +2,6 @@
 #define __IDK__
 #include <stdint.h>
 // TODO: Make this general.
-#include "../../../kallsymlib/kallsymlib.h"
 void * sym_get_fn_address(char *symbol);
 extern void sym_l2_init();
 #endif
--- src/LIDK/idk.c
+++ src/LIDK/idk.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include "LIDK/idk.h"
 #include <stdint.h>
+#include <string.h>
 
 #include "L2/sym_lib_page_fault.h"
 #include "L2/sym_probe.h"
 
-void* sym_get_fn_address(char *symbol){
-  // Elevate to be able to call kallsyms_lookup_name
-  sym_elevate();  
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+static kallsyms_lookup_name_t kallsyms_lookup_name = NULL;
 
-  struct kallsymlib_info info;
+void parse_system_map() {
+	const char* system_map_path = "/boot/System.map-5.14.0-symbiote+";
 
-  if (!kallsymlib_lookup(symbol, &info)) {
-    fprintf(stderr, "%s : not found\n", symbol);
-    while(1);
+	unsigned long long addr;
+	char type;
+	char symbol[128];
+
+	FILE* fp = fopen(system_map_path, "r");
+	if (fp == NULL) {
+		perror(system_map_path);
+		return;
+	}
+
+	int n;
+	while ((n = fscanf(fp, "%llx %c %s", &addr, &type, symbol)) != EOF) {
+		if (strcmp(symbol, "kallsyms_lookup_name") == 0) {
+			kallsyms_lookup_name = (kallsyms_lookup_name_t)addr;
+			break;
+		}
+	}
+
+	fclose(fp);
+}
+
+void* sym_get_fn_address(char *symbol) {
+  // Initialization step: parse system map to get
+  // the address of the kallsyms_lookup_name routine.
+  if (kallsyms_lookup_name == NULL) {
+	parse_system_map();
   }
+
+  // Elevate to be able to call kallsyms_lookup_name
+  sym_elevate();
+
+  void* result = (void*)kallsyms_lookup_name(symbol);
 
   // Don't forget to lower
   sym_lower();
 
-  return (void*)info.addr;
+  return (void*)result;
 }
 
 void sym_l2_init(){
