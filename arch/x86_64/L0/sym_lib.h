3ad8b04d0b808f94f42eb27539277d1d4beb273e
e-arlo-be <arloalb@bu.edu>
2022-12-01 12:31:44 -0500

    add implementation of swapgs to work on moc

--- arch/x86_64/L0/sym_lib.h
+++ arch/x86_64/L0/sym_lib.h
@@ -5,12 +5,11 @@
 /* #include <sys/syscall.h> */
 
 /* #include "L0/sym_lib.h" */
-#define ARLO_GS
 //TODO: make this save for migrated processes. As it stands a migrated symbiote will have a gs base pointer into the wrong core
-#ifdef ARLO_GS
 #define GET_KERN_GS_CLOBBER_USER_GS                           \
   asm("movl $0xc0000102, %%ecx; rdmsr; movl $0xc0000101, %%ecx; wrmsr; sti;" :: :"%rax", "%edx", "%ecx");
-#else
+
+#if 0
 // get onto kern gs
 // Store kern gs
 // get onto user gs
@@ -22,6 +21,4 @@
   asm("swapgs");                                              \
   asm("wrgsbase %0" :: "r"(kern_gs) );                        \
   asm("sti");
-
-#endif //ARLO_GS
 #endif
