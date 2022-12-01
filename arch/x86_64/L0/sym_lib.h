01fa6c8e14906d8117e18114cf5edf8d8524bcb9
e-arlo-be <arloalb@bu.edu>
2022-12-01 12:40:21 -0500

    fix bug, clean up code

--- arch/x86_64/L0/sym_lib.h
+++ arch/x86_64/L0/sym_lib.h
@@ -6,19 +6,8 @@
 
 /* #include "L0/sym_lib.h" */
 //TODO: make this save for migrated processes. As it stands a migrated symbiote will have a gs base pointer into the wrong core
+
 #define GET_KERN_GS_CLOBBER_USER_GS                           \
   asm("movl $0xc0000102, %%ecx; rdmsr; movl $0xc0000101, %%ecx; wrmsr; sti;" :: :"%rax", "%edx", "%ecx");
 
-#if 0
-// get onto kern gs
-// Store kern gs
-// get onto user gs
-// Overwrite user gs with kern gs
-// make interruptable
-#define GET_KERN_GS_CLOBBER_USER_GS                           \
-  asm("swapgs");                                              \
-  asm("rdgsbase %0" : "=rm"(kern_gs) :: );                    \
-  asm("swapgs");                                              \
-  asm("wrgsbase %0" :: "r"(kern_gs) );                        \
-  asm("sti");
-#endif
+#endif //__ARCH_X86_64_SYM_LIB__
