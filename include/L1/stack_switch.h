5615a72ae46ae0f502f7d723a4344c536c398fed
Tommy Unger <tunger10@gmail.com>
2023-03-06 10:54:08 -0500

    Kern stack one liner

--- include/L1/stack_switch.h
+++ include/L1/stack_switch.h
@@ -5,4 +5,21 @@
 #include "../../arch/x86_64/L1/stack_switch.h"
 #endif
 
+// These must be used as a pair
+#define SYM_ON_KERN_STACK() \
+    sym_elevate(); \
+    uint64_t user_stack; \
+    SYM_PRESERVE_USER_STACK(user_stack); \
+    SYM_SWITCH_TO_KERN_STACK();
+
+#define SYM_ON_USER_STACK() \
+    SYM_RESTORE_USER_STACK(user_stack); \
+    sym_lower();
+
+// Combine the two above so we don't have to remember to call both
+// but put all of user code inbetween
+#define SYM_ON_KERN_STACK_DO(fn) \
+    SYM_ON_KERN_STACK(); \
+    fn; \
+    SYM_ON_USER_STACK();
 #endif
